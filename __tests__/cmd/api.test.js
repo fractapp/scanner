da0aecfcf4ab18b752a3197363ecbea4c7ff60c5
yura6904 <biryulinyura@mail.ru>
2021-07-26 18:32:41 +0300

    app left to fix a little bit

--- __tests__/cmd/api.test.js
+++ __tests__/cmd/api.test.js
@@ -1,10 +1,10 @@
 import { BlockStatus, Network, TxStatus } from '../../src/models/enums/statuses';
 import { Currency } from '../../src/models/enums/currency';
 import { SubstrateAdaptor } from '../../src/adaptors/substrate';
+import request from "supertest"
 
-const app = require('../../src/cmd/api')
-const supertest = require('supertest')
-const request = supertest(app)
+const app = require('../../src/cmd/api');
+const supertest = require('supertest');
 const mongoose = require('mongoose');
 const mockingoose = require('mockingoose');
 const modelBlock = require('../../src/models/db/block');
@@ -22,14 +22,7 @@ jest.mock('../../src/adaptors/substrate', () => ({
     SubstrateAdaptor: {
         getInstance: jest.fn(),
     },
-}));/*
-jest.mock('mongoose', () => ({
-    ...jest.requireActual('mongoose'),
-    find: jest.fn(() => ({
-        populate: jest.fn(),
-        sort: jest.fn(),
-    })),
-}));*/
+}));
 jest.mock('../../src/adaptors/adaptor', () => ({
     Adaptor: {
         getBalance: jest.fn(),
@@ -43,131 +36,170 @@ beforeAll(async function() {
         getLastHeight: jest.fn(() => 231n), 
     });
 });
+beforeAll(done => {
+    done()
+  })
+  
+  afterAll(done => {
+    // Closing the DB connection allows Jest to exit successfully.
+    mongoose.connection.close()
+    done()
+  })
+
 it('test api 1', async () => {
-    const block1 = await modelBlock.Block.create(
-        {
-            _id: mongoose.Types.ObjectId(),
-            hash: 'hash1',
-            number: '125',
-            status: BlockStatus.Success,
-            network: Network.Polkadot,
-            isNotified: false,
-        }
-    )
-    const block2 = await modelBlock.Block.create(
-        {
-            _id: mongoose.Types.ObjectId(),
-            hash: 'hash2',
-            number: '126',
-            status: BlockStatus.Forked,
-            network: Network.Polkadot,
-            isNotified: false,
-        }
-    )
-    const tx1 = await modelTransaction.Transaction.create(
-        {
-            txId: 'txId',
-            hash: 'hash',
-            status: TxStatus.Success,
-            block: block1,
-        }
-    );
-    const tx2 = await modelTransaction.Transaction.create(
-        {
-            txId: 'txId',
-            hash: 'hash',
-            status: TxStatus.Success,
-            block: block2,
-        }
-    );
-    const txs = [tx1, tx2];
-    const spy = jest.spyOn(modelTransaction.Transaction, 'find');
-    jest.spyOn(modelEvent.Event, 'find').mockReturnValue(txs);
-
-    const response = await request.get('/transaction/:hash');
-
-    expect(spy).toBeCalledTimes(1);
-    expect(response.status).toBe(200);
-});
-
-it('test api 2', async () => {
-    const block = await modelBlock.Block.create(
-        {
-            _id: mongoose.Types.ObjectId(),
-            hash: 'hash2',
-            number: '126',
-            status: BlockStatus.Success,
-            network: Network.Polkadot,
-            isNotified: false,
-        }
-    )
-    const tx = await modelTransaction.Transaction.create(
-        {
-            txId: 'txId',
-            hash: 'hash',
-            status: TxStatus.Success,
-            block: block,
-        }
-    );
-    const event = {
-        eventId: 'eventId',
-        block: block,
-        transaction: tx,
-        from: 'from',
-        to: 'to',
-        fee: 'fee',
-        value: 'value',
-        timestamp: 12345,
-        currency: Currency.DOT,
-        isNotified: false,
-    };
-    const req = {
-        address: 'address',
-        page: 2,
-        currency: Currency.DOT,
-    };
-    const spy = jest.spyOn(modelEvent.Event, 'find');
-
-    jest.spyOn(modelEvent.Event, 'find').mockReturnValue([
-        event
-    ]);
-    //await mockingoose(modelEvent.Event).toReturn([event], 'find');
-    const response = await request.get('/transactions/:address', req);
-    
-    expect(spy).toBeCalledTimes(1);
-    expect(response.text).toBe(1);
-});
-
-it('test api 3', async () => {
-    const req = {
-        address: 'address',
-        page: 2,
-        currency: Currency.DOT,
-    };
-    
-    const response = await request.get('/substrate/balance/:address', req);
-
-    expect(response.text).toBe("{\"value\":\"231\"}");
-});
-
-it('test api 4', async () => {    
-    const block = {
-        _id: mongoose.Types.ObjectId(),
+    const block1 = {
         hash: 'hash',
         number: '125',
         status: BlockStatus.Success,
         network: Network.Polkadot,
         isNotified: false,
+    }
+    
+    const block2 = {
+        hash: 'hash2',
+        number: '126',
+        status: BlockStatus.Success,
+        network: Network.Polkadot,
+        isNotified: false,
     };
-    const spy = jest.spyOn(modelBlock.Block, 'findOne');
-    jest.spyOn(modelBlock.Block, 'findOne').mockReturnValue(block);
-    jest.spyOn(modelBlock.Block, 'findOne').mockReturnValue(block);
-    jest.spyOn(modelBlock.Block, 'findOne').mockReturnValue(block);
-    jest.spyOn(modelBlock.Block, 'findOne').mockReturnValue(block);
+    
+    const tx1 = {
+        txId: 'txId',
+        hash: 'hash',
+        status: 1,
+        block: block1,
+    };
+    const tx2 = {
+        txId: 'txId',
+        hash: 'hash2',
+        status: 1,
+        block: block2,
+    };
+    const txs = [tx1, tx2];
+    
+    const spyFind = jest.spyOn(modelTransaction.Transaction, 'find').mockReturnValueOnce({
+        populate: jest.fn(() => ({
+            sort: jest.fn(() => txs)
+        })),
+    });
 
-    const response = await request.get('/status');
+    const response = await request(app).get('/transaction/:hash');
+
+    expect(spyFind).toBeCalledTimes(1);
+    expect(response.status).toBe(200);
+});
+
+
+it('test api 2', async () => {
+    const block1 = {
+        hash: 'hash2',
+        number: '126',
+        status: BlockStatus.Success,
+        network: Network.Polkadot,
+        isNotified: false,
+    };    
+    const block2 = {
+        hash: 'hash2',
+        number: '126',
+        status: BlockStatus.Pending,
+        network: Network.Polkadot,
+        isNotified: false,
+    };
+    const tx1 = {
+        txId: 'txId',
+        hash: 'hash',
+        status: TxStatus.Success,
+        block: block1,
+    };
+    const tx2 = {
+        txId: 'txId2',
+        hash: 'hash2',
+        status: TxStatus.Success,
+        block: block2,
+    };
+
+    const events = [
+        {
+            eventId: 'eventId',
+            block: block1,
+            transaction: tx1,
+            from: 'from',
+            to: 'to',
+            fee: 'fee',
+            value: 'value',
+            timestamp: 12345,
+            currency: Currency.DOT,
+            isNotified: false,
+        },
+        {
+            eventId: 'eventId2',
+            block: block2,
+            transaction: tx2,
+            from: 'from1',
+            to: 'to1',
+            fee: 'fee1',
+            value: 'value1',
+            timestamp: 12345,
+            currency: Currency.DOT,
+            isNotified: false,
+        },
+
+    ];
+    const spy = jest.spyOn(modelEvent.Event, 'find');
+
+    jest.spyOn(modelEvent.Event, 'find').mockReturnValueOnce({
+        populate: jest.fn(() => ({
+            populate: jest.fn(() => ({
+                sort: jest.fn(() => ({
+                    skip: jest.fn(() => ({
+                        limit: jest.fn(() => events)
+                    })),
+                })),
+            })),
+        })),
+    });
+
+    const response = await request(app)
+      .get('/transactions/:address')
+      .query({
+        page: '2',
+        size: '1111'
+    });
+    expect(response.statusCode).toEqual(200)
+    expect(spy).toBeCalledTimes(1);
+    expect(response.text).toStrictEqual("[{\"id\":\"eventId\",\"hash\":\"hash\",\"currency\":0,\"to\":\"to\",\"from\":\"from\",\"value\":\"value\",\"fee\":\"fee\",\"timestamp\":12345,\"status\":1}]");
+});
+
+it('test api 3', async () => {
+    const response = await request(app).get('/substrate/balance/:address');
+    expect(response.text).toBe("{\"value\":\"231\"}");
+});
+
+it('test api 4', async () => {    
+    const block = {
+        hash: 'hash2',
+        number: '126',
+        status: BlockStatus.Success,
+        network: Network.Polkadot,
+        isNotified: false,
+    };
+   
+    const spy = jest.spyOn(modelBlock.Block, 'findOne');
+    jest.spyOn(modelBlock.Block, 'findOne').mockReturnValue({
+        sort: jest.fn(() => block)
+    });
+    jest.spyOn(modelBlock.Block, 'findOne').mockReturnValue({
+        sort: jest.fn(() => block)
+    });
+    jest.spyOn(modelBlock.Block, 'findOne').mockReturnValue({
+        sort: jest.fn(() => block)
+    });
+    jest.spyOn(modelBlock.Block, 'findOne').mockReturnValue({
+        sort: jest.fn(() => block)
+    });
+    const response = await request(app).get('/status');
 
     expect(spy).toBeCalledTimes(4);
-    expect(response.text).toBe("{\"polkadot\":{\"lastHeight\":\"231\",\"lastScannedHeight\":\"0\",\"lastNotifiedHeight\":\"0\"},\"kusama\":{\"lastHeight\":\"231\",\"lastScannedHeight\":\"0\",\"lastNotifiedHeight\":\"0\"}}");
+    expect(response.text).toBe("{\"polkadot\":{\"lastHeight\":\"231\",\"lastScannedHeight\":\"126\",\"lastNotifiedHeight\":\"126\"},\"kusama\":{\"lastHeight\":\"231\",\"lastScannedHeight\":\"126\",\"lastNotifiedHeight\":\"126\"}}");
 });
 
--- __tests__/cmd/scanNotifier.test.js
+++ __tests__/cmd/scanNotifier.test.js
@@ -1,6 +1,8 @@
 import { scan } from '../../src/cmd/scanNotifier';
 import { Currency } from '../../src/models/enums/currency';
 import { BlockStatus, Network, TxStatus } from '../../src/models/enums/statuses';
+import axios from 'axios';
+import mockAxios from 'jest-mock-axios';
 
 const mongoose = require('mongoose');
 const mockingoose = require('mockingoose');
@@ -8,42 +10,47 @@ const modelBlock = require('../../src/models/db/block');
 const modelEvent = require('../../src/models/db/event');
 const modelTransaction = require('../../src/models/db/transactions');
 
+jest.mock('axios');
+
+afterEach(async () => {
+    mockAxios.reset();
+    jest.resetAllMocks();
+});
+
+
 it('test scanNotifier 1', async () => {
-    const block1 = await modelBlock.Block.create({
-        _id: mongoose.Types.ObjectId(),
+    const block = {
         hash: 'hash',
         number: '125',
         status: BlockStatus.Success,
         network: Network.Polkadot,
         isNotified: false
-    });
+    };
     
-    const spy = jest.spyOn(modelBlock.Block, 'findOne');
-    jest.spyOn(modelBlock.Block, 'findOne').mockReturnValue(block1);
+    const spy = jest.spyOn(modelBlock.Block, 'findOne').mockReturnValueOnce({
+        sort: jest.fn(() => block)
+    });
 
-    expect(await scan('url', 123n, Network.Polkadot)).toStrictEqual(123n);
-    expect(spy).toBeCalledTimes(2);
+    expect(await scan('url', 123n, Network.Polkadot)).toStrictEqual(125n);
+    expect(spy).toBeCalledTimes(3);
 });
 it('test scanNotifier 2', async () => {
-    const block = await modelBlock.Block.create(
-        {
-            _id: mongoose.Types.ObjectId(),
-            hash: 'hash2',
-            number: '126',
-            status: BlockStatus.Success,
-            network: Network.Polkadot,
-            isNotified: false,
-        }
-    )
-    const tx = await modelTransaction.Transaction.create(
-        {
-            txId: 'txId',
-            hash: 'hash',
-            status: TxStatus.Success,
-            block: block,
-        }
-    );
-    const event = await modelEvent.Event.create({
+    const block = {
+        hash: 'hash2',
+        number: '126',
+        status: BlockStatus.Success,
+        network: Network.Polkadot,
+        isNotified: false,
+        save: jest.fn(async () => {})
+
+    }
+    const tx ={
+        txId: 'txId',
+        hash: 'hash',
+        status: TxStatus.Fail,
+        block: block,
+    }
+    const event = {
         eventId: 'eventId',
         block: block,
         transaction:  tx,
@@ -53,22 +60,115 @@ it('test scanNotifier 2', async () => {
         value: 'value',
         timestamp: 12345,
         currency: Currency.DOT,
-        isNotified: false
-    });    
+        isNotified: false,
+        save: jest.fn(async () => {})
+    };    
 
-    const spyBlock = jest.spyOn(modelBlock.Block, 'findOne');
-    jest.spyOn(modelBlock.Block, 'findOne').mockReturnValue(block);
+    const spyBlock = jest.spyOn(modelBlock.Block, 'findOne')
+    .mockReturnValueOnce({
+        sort: jest.fn(() => block)
+    })
+    .mockReturnValueOnce(block);
+    const spyEvent = jest.spyOn(modelEvent.Event, 'find').mockReturnValueOnce({
+        populate: jest.fn(() => [event])
+    });
 
-    const spyEvent = jest.spyOn(modelEvent.Event, 'find');
-    jest.spyOn(modelEvent.Event, 'find').mockReturnValue([event]);
-
-    expect(await scan('url', 123n, Network.Polkadot)).toStrictEqual(123n);
-    expect(spyBlock).toBeCalledTimes(1);
+    expect(await scan('url1', 123n, Network.Polkadot)).toStrictEqual(126n);
+    expect(spyBlock).toBeCalledTimes(4);
     expect(spyEvent).toBeCalledTimes(1);
 });
+
 it('test scanNotifier 3', async () => {
-    const spyBlock = jest.spyOn(modelBlock.Block, 'findOne');
-    jest.spyOn(modelBlock.Block, 'findOne').mockReturnValue({});
+    const block = {
+        hash: 'hash2',
+        number: '126',
+        status: BlockStatus.Success,
+        network: Network.Polkadot,
+        isNotified: false,
+        save: jest.fn(async () => {})
+    }
+    const tx = {
+        txId: 'txId',
+        hash: 'hash',
+        status: TxStatus.Success,
+        block: block,
+    }
+    const event = {
+        eventId: 'eventId',
+        block: block,
+        transaction:  tx,
+        from: 'from',
+        to: 'to',
+        fee: 'fee',
+        value: 'value',
+        timestamp: 12345,
+        currency: Currency.DOT,
+        isNotified: false,
+        save: jest.fn(async () => {})
+    };    
+
+    const spyBlock = jest.spyOn(modelBlock.Block, 'findOne')
+    .mockReturnValueOnce({
+        sort: jest.fn(() => block)
+    })
+    .mockReturnValueOnce(block);
+    const spyEvent = jest.spyOn(modelEvent.Event, 'find').mockReturnValueOnce({
+        populate: jest.fn(() => [event])
+    });
+    await axios.post.mockResolvedValue({status: 400});
+
+    expect(await scan('url', 123n, Network.Polkadot)).toStrictEqual(126n);
+    expect(spyBlock).toBeCalledTimes(5);
+    expect(spyEvent).toBeCalledTimes(1);
+});
+it('test scanNotifier 4', async () => {
+    const block = {
+        hash: 'hash2',
+        number: '126',
+        status: BlockStatus.Success,
+        network: Network.Polkadot,
+        isNotified: false,
+        save: jest.fn(async () => {})
+    }
+    const tx = {
+        txId: 'txId',
+        hash: 'hash',
+        status: TxStatus.Success,
+        block: block,
+    }
+    const event = {
+        eventId: 'eventId',
+        block: block,
+        transaction:  tx,
+        from: 'from',
+        to: 'to',
+        fee: 'fee',
+        value: 'value',
+        timestamp: 12345,
+        currency: Currency.DOT,
+        isNotified: false,
+        save: jest.fn(async () => {})
+    };    
+
+    const spyBlock = jest.spyOn(modelBlock.Block, 'findOne')
+    .mockReturnValueOnce({
+        sort: jest.fn(() => block)
+    })
+    .mockReturnValueOnce(block);
+    const spyEvent = jest.spyOn(modelEvent.Event, 'find').mockReturnValueOnce({
+        populate: jest.fn(() => [event])
+    });
+    await axios.post.mockResolvedValue({status: 200});
+
+    expect(await scan('url', 123n, Network.Polkadot)).toStrictEqual(126n);
+    expect(spyBlock).toBeCalledTimes(4);
+    expect(spyEvent).toBeCalledTimes(1);
+});
+
+it('test scanNotifier 5', async () => {
+    const spyBlock = jest.spyOn(modelBlock.Block, 'findOne').mockReturnValueOnce({
+        sort: jest.fn(() => {})
+    });
     
     expect(await scan('url', 123n, Network.Polkadot)).toStrictEqual(123n);
     expect(spyBlock).toBeCalledTimes(1);
